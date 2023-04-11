0c72961b59cb69c3eaf6acefe7a4f4c4609c4507
Dilip Kumar <108999605+Dilip-ku5@users.noreply.github.com>
2023-04-11 21:31:57 +0530

    Add files via upload

---
+++ c advance/file.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+void main(){
+	char c ;
+	FILE *f;
+	f = fopen("abc.text","w");
+	if(f==NULL){
+		printf("Error");
+		exit(0);
+	}
+	else
+		while((c=getchar())!=EOF){
+			putc(c,f);
+
+
+
+
+
+		 getch();
+		}
+
+
+
+
+
+
+
+
+}
